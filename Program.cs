2ad4ac9572b2dca8a1ac99455e3c35dca6faac08
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-03-18 11:23:16 +0200

    Moria entrance scene

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Врата Дарина. Молви друг и входи.");
+            string userPIN = Console.ReadLine();
+            if (userPIN == "Мелон")
+            {
+                Console.WriteLine("Как тебя зовут, друг?");
+                string userNAME = Console.ReadLine();
+                Console.WriteLine($"Проходи же, {userNAME}!");
+
+            }
+            else
+            {
+            Console.WriteLine("Ты не пройдёшь!");
+            }
+
+        }
+    }
+}
